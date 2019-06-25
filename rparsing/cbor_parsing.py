4433e386aab563f52ad93f4e6c699f7810e097ce
jramsdell <ramsdell.jordan@gmail.com>
2019-06-25 12:01:35 -0400

    Update parsing script with comments

--- rparsing/cbor_parsing.py
+++ rparsing/cbor_parsing.py
@@ -9,37 +9,49 @@ from collections import Counter
 from trec_car.read_data import iter_outlines, iter_paragraphs, ParaLink, ParaText
 import sys
 
+# Removed labelState
+# facetState now has "relevance" value
 
+# Hidden state will be renamed to nonrelevantState
+# Won't have a second value ('false')
+
+
+# ---------------------------- CBOR Outline Parser ----------------------------
 class OutlineReader(object):
-    page_title_map = ...  # type: Dict[str, str]
-    section_heading_map = ...  # type: Dict[str, List[str]]
-    section_id_map = ...  # type: Dict[str, str]
+    page_title_map = ...                    # type: Dict[str, str]
+    page_toplevel_section_names = ...       # type: Dict[str, List[str]]
+    page_toplevel_section_ids = ...         # type: Dict[str, str]
 
     def __init__(self, f):
+        """
+        :type outline_loc: Location of .cbor outline file
+        """
         self.page_title_map = {}
-        self.section_heading_map = {}
-        self.section_id_map = {}
+        self.page_toplevel_section_names = {}
+        self.page_toplevel_section_ids = {}
 
+        # Iterate of .cbor file (each item is a page outline)
         for outline in iter_outlines(f):
             self.page_title_map[outline.page_id] = outline.page_name
-            hmap = []
-            imap = []
+            toplevel_section_names = []
+            toplevel_section_ids = []
 
-            for s in outline.child_sections:
-                hmap.append(s.heading)
-                id = outline.page_id + "/" + s.headingId
-                imap.append(id)
-                self.page_title_map[id] = s.heading
+            # Contains the top-level sections as children of the page outline
+            for section_outline in outline.child_sections:
+                toplevel_section_names.append(section_outline.heading)
+                id = outline.page_id + "/" + section_outline.headingId
+                toplevel_section_ids.append(id)
+                self.page_title_map[id] = section_outline.heading
 
-
-            self.section_heading_map[outline.page_id] = hmap
-            self.section_id_map[outline.page_id] = imap
-
-
+            self.page_toplevel_section_names[outline.page_id] = toplevel_section_names
+            self.page_toplevel_section_ids[outline.page_id] = toplevel_section_ids
 
 
-
+# ---------------------------- JSON Data Structueres ----------------------------
 class Jsonable(object):
+    """
+    Convenience class that contains method to convert attributes of a class into a json.
+    """
     def to_json(self):
         return json.dumps(self, default=lambda o: o.__dict__,
                           sort_keys=True, indent=4)
@@ -50,11 +62,17 @@ class Section(Jsonable):
 
 
 class ParBody(Jsonable):
+    """
+    Represents the text of a paragraph.
+    """
     def __init__(self):
         self.text = "Test"
 
 
 class Paragraph(Jsonable):
+    """
+    Paragraph container that contains links / paragraph text. Is updated using ParagraphTextcollector class.
+    """
     para_body = ...  # type: List[ParBody]
 
     def __init__(self, paraId):
@@ -68,18 +86,33 @@ class Paragraph(Jsonable):
 
 
 class QueryFacet(Jsonable):
+    """
+    An annotation query facet (containing the facet's name and id)
+    """
     def __init__(self, heading_id, heading):
         self.heading_id = heading_id
         self.heading = heading
 
 class ParagraphOrigin(Jsonable):
+    """
+    Contains information about the origin of a paragraph
+    """
     def __init__(self, para_id, section_path, rank_score, rank):
+        """
+        :param para_id:         ID of the paragraph
+        :param section_path:    The toplevel section that the paragraph is contained in
+        :param rank_score:      The score of the paragraph when it was retrieved
+        :param rank:            The rank of the paragraph when it was retrieved
+        """
         self.para_id = para_id
         self.section_path = section_path
         self.rank_score = rank_score
         self.rank = rank
 
 class Page(Jsonable):
+    """
+    A page used for annotations.
+    """
     query_facets = ...  # type: List[QueryFacet]
     paragraphs = ... # type: List[Paragraph]
     paragraph_origins = ... # type: List[ParagraphOrigin]
@@ -108,6 +141,9 @@ class Page(Jsonable):
             self.paragraphs.append(paragraph)
 
     def write_self(self):
+        """
+        Calls to_json recursively on all of the page's components and then writes contents to a file.
+        """
         out_name = "jsons/" + self.run_id + "_" + self.title.replace(" ", "_") + ".json"
         if not os.path.exists("jsons/"):
             os.mkdir("jsons/")
@@ -115,20 +151,58 @@ class Page(Jsonable):
         with open(out_name, "w") as f:
             f.write(self.to_json())
 
-class RunLine(object):
-    def __init__(self, line):
-        e = line.split()
-        self.qid = e[0]
-        self.doc_id = e[2]
-        self.rank = int(e[3])
-        self.score = float(e[4])
-        self.run_name = e[5]
 
-    def jsonify(self):
-        pass
 
+
+
+# class MisoAnnotationReader(object):
+#     def __init__(self, anno_loc):
+#         with open (anno_loc) as f:
+#             saved_data = self.parse_annotations(f)
+#             print(saved_data.keys())
+#
+#         query_facet_map = self.create_facet_map(saved_data)
+#         print(query_facet_map)
+#
+#     def create_facet_map(self, saved_data):
+#         facets = saved_data["facetState"]
+#         query_facet_map = {}
+#         for (entry, facet) in facets:
+#             pid = entry["paragraphId"]
+#             qid = entry["queryId"]
+#             heading = facet["heading"]
+#
+#             if qid not in query_facet_map:
+#                 query_facet_map[qid] = {}
+#             facet_map = query_facet_map[qid]
+#
+#             if heading not in facet_map:
+#                 facet_map[heading] = set()
+#             facet_map[heading].add(pid)
+#
+#         return query_facet_map
+#
+#
+#     def create_transition_map(self, saved_data):
+#         pass
+#
+#
+#
+#     def parse_annotations(self, f):
+#         json_text = f.read()
+#         annotations = json.loads(json_text)
+#         return annotations["savedData"]
+
+
+
+# ---------------------------- Run Parsing ----------------------------
 
 class RunManager(object):
+    """
+    Responsible for all the heavy lifting:
+     - Parses a directory full of runfiles.
+     - Creates data classes (that can be turned into jsons) based on these runs
+    """
     paragraphs_to_retrieve = ...  # type: Dict[str, List[Paragraph]]
     runs = ... # type: List[RunReader]
     pages = ...  # type: Dict[Tuple[str, str], Page]
@@ -150,24 +224,24 @@ class RunManager(object):
                 self.parse_run_line(run_line)
 
 
-
-
-
-
-    def parse_run_line(self, run_line: RunLine):
+    def parse_run_line(self, run_line: 'RunLine'):
         e = run_line.qid.split("/")
+
+        # Skip queries that are not top-level!
         if len(e) != 2:
             return
 
-        tid = e[0]
+        tid = e[0]  # toplevel query ID
         key = (run_line.run_name, tid)
 
+        # The first time we see a toplevel query for a particular run, we need to initialize a jsonable page
         if key not in self.pages:
             p = Page()
             p.squid = tid
             p.title = self.oreader.page_title_map[tid]
             p.run_id = run_line.run_name
-            for (f_heading, f_id) in zip(self.oreader.section_heading_map[tid], self.oreader.section_id_map[tid]):
+            for (f_heading, f_id) in zip(self.oreader.page_toplevel_section_names[tid],
+                                         self.oreader.page_toplevel_section_ids[tid]):
                 qf = QueryFacet(heading=f_heading, heading_id=f_id)
                 p.query_facets.append(qf)
             self.pages[key] = p
@@ -190,6 +264,11 @@ class RunManager(object):
         page.paragraph_origins.append(origin)
 
     def register_paragraph(self, paragraph: Paragraph):
+        """
+        Remember where this paragraph is for later when we have to parse a paragraphCorpus.cbor file.
+        We will be adding the parsed text to this paragraph. Since there can be multiple instances of this paragraph
+        among queries and runs, they are all stored in a map of lists for later text retrieval.
+        """
         key = paragraph.para_id
         if key not in self.paragraphs_to_retrieve:
             self.paragraphs_to_retrieve[key] = []
@@ -197,20 +276,28 @@ class RunManager(object):
 
 
     def retrieve_text(self, cbor_loc):
+        """
+        Passes all registered paragraphs to the ParagraphTextCollector for text retrieval.
+        :param cbor_loc: Location to paragraphCorpus.cbor file
+        """
         pcollector = ParagraphTextCollector(self.paragraphs_to_retrieve)
-        # for run in self.runs:
-        #     pcollector.collect_pids(run)
+        pcollector.retrieve_paragraph_mappings(cbor_loc)
 
-        text_mappings = pcollector.retrieve_paragraph_mappings(cbor_loc)
-        # print("Found {} out of {}".format(len(pcollector.text_mappings), len(pcollector.pids)))
 
-        # for page in self.pages.values():
-        #     for paragraph in page.paragraphs:
-        #          # TODO: change this when we implement entity links in parabody
-        #         paragraph.paraBody[0].text = text_mappings[paragraph.paraId]
+class RunLine(object):
+    def __init__(self, line):
+        e = line.split()
+        self.qid = e[0]             # Query ID
+        self.doc_id = e[2]          # Paragraph ID
+        self.rank = int(e[3])       # Rank of retrieved paragraph
+        self.score = float(e[4])    # Score of retrieved paragraph
+        self.run_name = e[5]        # Name of the run
 
 
 class RunReader(object):
+    """
+    Responsible for reading a single runfile, line-by-line, and storing them in RunLine data classes.
+    """
     runlines = ...  # type: List[RunLine]
 
     def __init__(self, run_loc, nlines):
@@ -222,6 +309,8 @@ class RunReader(object):
             for line in f:
                 run_line = RunLine(line)
                 self.run_counts[run_line.qid] += 1
+
+                # Only store the top N retrieved paragraphs from each query
                 if self.run_counts[run_line.qid] > nlines:
                     continue
                 self.seen_pids.add(run_line.doc_id)
@@ -232,44 +321,17 @@ class RunReader(object):
 
 
 class ParagraphTextCollector(object):
+    """
+    Retrieves text from paragraphCorpus.cbor file and adds it to the corresponding paragrpahs
+    """
     def __init__(self, paragraphs_to_retrieve):
         self.paragraphs_to_retrieve = paragraphs_to_retrieve
 
-    # def collect_paragraphs(self, manager: RunManager):
-    #     for page in run_manager.pages
-    #     self.pids = self.pids.union(run.seen_pids)
-
-    # def retrieve_json_mappings(self, json_loc):
-    #     with open("backup.json") as f:
-    #         text = f.read()
-    #     text_mappings = json.loads(text)
-    #     return text_mappings
-    #
-    # def retrieve_cbor_mappings(self, cbor_loc):
-    #     counter = 0
-    #     with open(cbor_loc, 'rb') as f:
-    #         for p in iter_paragraphs(f):
-    #             counter += 1
-    #             if counter % 100000 == 0:
-    #                 print(counter)
-    #             if p.para_id in self.pids:
-    #                 self.text_mappings[p.para_id] = p.get_text()
-    #
-    #     with open("backup.json", "w") as f:
-    #         f.write(json.dumps(self.text_mappings))
-    #     return self.text_mappings
-    #
-    # def retrieve_text(self, file_loc):
-    #     extension = file_loc.split(".")[-1].lower()
-    #     if extension == "cbor":
-    #         return self.retrieve_cbor_mappings(file_loc)
-    #     elif extension == "json":
-    #         return self.retrieve_json_mappings(file_loc)
-    #
-    #     raise Exception("unknown file extension (.{}). Expected .json or .cbor".format(extension))
-
 
     def retrieve_paragraph_mappings(self, cbor_loc):
+        """
+        :param cbor_loc: Location of the paragraphCorpus.cbor file
+        """
         counter = 0
         seen = 0
         total = len(self.paragraphs_to_retrieve)
@@ -287,12 +349,11 @@ class ParagraphTextCollector(object):
                     if seen == total:
                         break
 
-        # with open("backup.json", "w") as f:
-        #     f.write(json.dumps(self.text_mappings))
-        # return self.text_mappings
-
-
     def update_paragraph(self, p: Paragraph, pbodies):
+        """
+        :param p_to_be_updated: Paragraph that we will be updating
+        :param pbodies:
+        """
         for e in pbodies:
             body = ParBody()
 
@@ -326,77 +387,32 @@ def get_parser():
     parsed = parser.parse_args()
     return parsed.__dict__
 
-def test(cbor_loc):
-    with open(cbor_loc, 'rb') as f:
-        p = next(iter_paragraphs(f))
-        print(p.bodies)
-        for e in p.bodies:
-            if isinstance(e, ParaLink):
-                print(e.page)
-                print(e.get_text())
-                print(e.anchor_text)
+# def test(cbor_loc):
+#     with open(cbor_loc, 'rb') as f:
+#         p = next(iter_paragraphs(f))
+#         print(p.bodies)
+#         for e in p.bodies:
+#             if isinstance(e, ParaLink):
+#                 print(e.page)
+#                 print(e.get_text())
+#                 print(e.anchor_text)
 
 
-
-if __name__ == '__main__':
+def run_parse():
     parsed = get_parser()
     outlines = parsed["outline_cbor"]
     run_loc = parsed["run_directory"]
     np = int(parsed["n"])
     cbor_loc = parsed["paragraph_cbor"]
-
-
-
-    # path = "/home/hcgs/PycharmProjects/run_parsing/benchmarkY2.cbor-outlines.cbor"
-    # run_loc = "/home/hcgs/data_science/data/y2_runs/guir"
-    # run_loc2 = "/home/hcgs/data_science/data/y2_runs"
-    # cbor_loc = "/home/hcgs/data_science/data/corpus/dedup.articles-paragraphs.cbor"
-
-    # with open(path, 'rb') as f:
-        # title_map, section_map = get_smap(f)
-        # oreader = OutlineReader(f)
-    # print(oreader.section_id_map)
-
-    # run = RunReader(run_loc)
-    # run_manager = RunManager("/home/hcgs/data_science/data/small_runs", path, nlines=20)
     run_manager = RunManager(run_loc, outlines, nlines=np)
     run_manager.retrieve_text(cbor_loc)
 
-    # for page in run_manager.pages.values():
-    #     for paragraph in page.paragraphs:
-    #         if paragraph.paraBody:
-    #             print(paragraph.to_json())
-    # for i in run_manager.pages.values():
-    #     for o in i.paragraph_origins:
-    #         pass
-    #     break
-    # print(run.runlines[0])
-
-    # p = Page()
-    # p.squid = "Haha"
-    # print(p.to_json())
-
-    # hah = ParagraphTextCollector()
-    # test(cbor_loc)
-    # print(hah.retrieve_text("blah.dah"))
-
-
-    # with open("backup.json") as f:
-    #     text = f.read()
-    #
-    # text_map = json.loads(text)
-
-
-
-    # for (k,v) in sorted(run_manager.pages.items(), key=lambda x: x[0]):
-    #     for paragraph in v.paragraphs:
-    #         paragraph.paraBody[0].text = text_map[paragraph.paraId]
-    #
-    #
     for (k,v) in sorted(run_manager.pages.items(), key=lambda x: x[0]):
         v.write_self()
 
 
+if __name__ == '__main__':
+    run_parse()
 
 
 
